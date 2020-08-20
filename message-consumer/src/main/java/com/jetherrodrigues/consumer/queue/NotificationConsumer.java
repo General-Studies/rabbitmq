02274963c9bc6ee74bb1d2a3769725ab37c2a100
Jether Rodrigues <jetherrodrigues@gmail.com>
2020-08-20 11:28:07 -0300

    removed @Component annotation

--- message-consumer/src/main/java/com/jetherrodrigues/consumer/queue/NotificationConsumer.java
+++ message-consumer/src/main/java/com/jetherrodrigues/consumer/queue/NotificationConsumer.java
@@ -3,9 +3,7 @@ package com.jetherrodrigues.consumer.queue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NotificationConsumer implements Consumer<UserMessage> {
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationConsumer.class);
