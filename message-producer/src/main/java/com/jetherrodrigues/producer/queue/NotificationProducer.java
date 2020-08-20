2a30f905c660c9088534bb778fec6c8a06dac5c3
Jether Rodrigues <jetherrodrigues@gmail.com>
2020-08-20 11:16:26 -0300

    removed @Component configuration

--- message-producer/src/main/java/com/jetherrodrigues/producer/queue/NotificationProducer.java
+++ message-producer/src/main/java/com/jetherrodrigues/producer/queue/NotificationProducer.java
@@ -7,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NotificationProducer implements Producer<User> {
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationProducer.class);
